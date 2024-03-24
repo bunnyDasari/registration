95d82ab352554ef419f3da8f6f4ebc72c62caa8f
rohandasari <rohandasari098@gmail.com>
2024-03-24 04:56:36 +0000

    registation

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import {NavBar, Logo} from './styled'
+
+const Navbar = () => (
+  <NavBar>
+    <Logo
+      src="https://assets.ccbp.in/frontend/react-js/meetup/website-logo-img.png"
+      alt="website logo"
+    />
+  </NavBar>
+)
+
+export default Navbar
